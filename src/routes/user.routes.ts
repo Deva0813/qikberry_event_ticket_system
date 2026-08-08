f80140e111b41f2cd01aed920225aad49ce5c92d
Deva <devanand151101@gmail.com>
2026-08-08 11:59:16 +0530

    Postman Collection Added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller.ts";
-import validate from '../middleware/validate.middleware.ts';
+import adminMiddleware from "../middleware/admin.middleware.ts";
+import authMiddleware from "../middleware/auth.middleware.ts";
+import validate from "../middleware/validate.middleware.ts";
 import { paramId } from "../validators/auth.validator.ts";
 
-const router = Router()
+const router = Router();
 
-router.patch('/user_role/:id', validate(paramId), userController.makeAdmin)
+router.patch(
+  "/user_role/:id",
+  authMiddleware,
+  adminMiddleware,
+  validate(paramId),
+  userController.makeAdmin,
+);
 
 export default router;
